eb6547b04d532473aac0840977da644cfb10d835
eleo95 <e.leonardo7474@gmail.com>
2018-09-10 00:46:53 -0400

    main activity design and different fragments created, menus and strings too!

--- app/src/main/java/com/eleo95/reportapp/MainActivity.java
+++ app/src/main/java/com/eleo95/reportapp/MainActivity.java
@@ -1,42 +1,122 @@
 package com.eleo95.reportapp;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.PopupMenu;
 import android.widget.Toast;
 
+import com.eleo95.reportapp.fragments.AccountFragment;
+import com.eleo95.reportapp.fragments.HomeFragment;
+import com.eleo95.reportapp.fragments.ReportsFragment;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
-    private boolean isLogged;
+    final Fragment homeFrag = new HomeFragment();
+    final Fragment reportFrag = new ReportsFragment();
+    final Fragment accFrag = new AccountFragment();
+    private Fragment selectedFragment = homeFrag;
+    private ImageView foto;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mAuth = FirebaseAuth.getInstance();
-       if(!isLogged){
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        foto = findViewById(R.id.photo);
 
-           Intent intent = new Intent(MainActivity.this,LogInActivity.class);
-           startActivity(intent);
-           finish();
+        if(currentUser != null){
+            Toast.makeText(this, "Welcome "+currentUser.getDisplayName(), Toast.LENGTH_SHORT).show();
+            getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, accFrag).hide(accFrag).commit();
+            getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, reportFrag).hide(reportFrag).commit();
+            getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, homeFrag).commit();
+            BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+            bottomNav.setOnNavigationItemSelectedListener(navListener);
+            foto.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                PopupMenu popupMenu = new PopupMenu(getApplicationContext(),foto);
+                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.about:
+                                // item one clicked
+                                return true;
+                            case R.id.salir:
+                                // item two clicked
+                                mAuth.signOut();
+                                Toast.makeText(getApplicationContext(),"Signed Out",Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(getApplicationContext(),LogInActivity.class);
+                                startActivity(intent);
+                                finish();
+                                return true;
+                        }
+                        return false;
+                    }
+                });
+                popupMenu.inflate(R.menu.avatar_menu);
+                popupMenu.show();
+                }
+            });
+
+
+
+        }else{
+            Intent intent = new Intent(MainActivity.this,LogInActivity.class);
+            startActivity(intent);
+            finish();
         }
 
+
     }
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+
+
+                    switch(item.getItemId()){
+                        case R.id.nav_home:
+                            getSupportFragmentManager().beginTransaction().hide(selectedFragment).show(homeFrag).commit();
+                            selectedFragment = homeFrag;
+                            foto.setVisibility(View.VISIBLE);
+                            break;
+                        case R.id.nav_report:
+                            getSupportFragmentManager().beginTransaction().hide(selectedFragment).show(reportFrag).commit();
+                            selectedFragment = reportFrag;
+                            foto.setVisibility(View.VISIBLE);
+                            break;
+                        case R.id.nav_account:
+                            getSupportFragmentManager().beginTransaction().hide(selectedFragment).show(accFrag).commit();
+                            selectedFragment = accFrag;
+                            foto.setVisibility(View.INVISIBLE);
+
+                            break;
+                    }
+
+
+                    return true;
+                }
+            };
 
     @Override
     public void onStart() {
         super.onStart();
         // Check if user is signed in (non-null) and update UI accordingly.
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        if(currentUser != null){
-            Toast.makeText(this, "nope", Toast.LENGTH_SHORT).show();
-            isLogged = true;
 
-        }else{
-            Toast.makeText(this, "hello", Toast.LENGTH_SHORT).show();
-        }
+
     }
 
 }
